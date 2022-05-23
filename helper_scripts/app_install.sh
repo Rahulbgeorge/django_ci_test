7740f1b237d1cd4fdd9fcd2609654782676c8ffd
Rahul B G <rahul.bgeorge@gmail.com>
2022-05-24 01:54:25 +0530

    app start

--- helper_scripts/app_install.sh
+++ helper_scripts/app_install.sh
@@ -4,7 +4,3 @@ sudo apt-get install -y python3
 sudo apt-get install -y python3-pip
 sudo apt-get install -y python-virtualenv
 sudo apt-get install -y virtualenv
-virtualenv -p python3 /home/ubuntu/venv
-sudo chmod 777 -R /home/ubuntu/venv
-/home/ubuntu/venv/bin/pip3 install -r /home/ubuntu/django_ci_test/requirements.txt
-
---
+++ helper_scripts/app_start.sh
@@ -0,0 +1,3 @@
+virtualenv -p python3 /home/ubuntu/venv
+sudo chmod 777 -R /home/ubuntu/venv
+/home/ubuntu/venv/bin/pip3 install -r /home/ubuntu/django_ci_test/requirements.txt
